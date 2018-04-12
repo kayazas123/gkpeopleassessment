a056a056430da2ab1c7e09706dd0111a3e8da636
Ayaz Ahmed <kayazas@gmail.com>
2018-04-12 16:11:23 +0200

    A

---
+++ src/main/java/com/gk/assessment/gkassessment/config/I18NConfig.java
@@ -0,0 +1,21 @@
+package com.gk.assessment.gkassessment.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+/**
+ * Created by AYAZ on 12/04/2018.
+ */
+@Configuration
+public class I18NConfig {
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource(){
+	ReloadableResourceBundleMessageSource reloadableResourceBundleMessageSource = new ReloadableResourceBundleMessageSource();
+	reloadableResourceBundleMessageSource.setBasename("classpath:i18n/messages");
+	//30 Minutes
+	reloadableResourceBundleMessageSource.setCacheSeconds(1800);
+	return reloadableResourceBundleMessageSource;
+    }
+}
--- src/test/java/com/gk/assessment/gkassessment/GkassessmentApplicationTests.java
+++ src/test/java/com/gk/assessment/gkassessment/GkassessmentApplicationTests.java
@@ -1,16 +1,32 @@
 package com.gk.assessment.gkassessment;
 
+import com.gk.assessment.gkassessment.web.i18n.I18NService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = GkassessmentApplication.class)
+@WebAppConfiguration
 public class GkassessmentApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Autowired
+    	private I18NService i18NService;
+
+    	@Test
+    	public void testMessageByLocaleService() throws Exception{
+	    String expectedResult = "Bootstrap starter template";
+	    String messageId = "index.main.callout";
+	    String actual = i18NService.getMessage(messageId);
+	    Assert.assertEquals("The actual and expected Strings dont match",expectedResult,actual);
 	}
 
 }
